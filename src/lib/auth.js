2e8ab87137edfdbf4df48913be34cc1e62508fae
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-02 19:33:35 +0600

    Install and setup better-auth with connect mongodb

---
+++ src/lib/auth.js
@@ -0,0 +1,17 @@
+import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
+import { mongodbAdapter } from "better-auth/adapters/mongodb";
+
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const db = client.db();
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true,
+    },
+
+    database: mongodbAdapter(db, {
+        client
+    }),
+});
